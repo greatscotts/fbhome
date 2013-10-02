98eb77ab39e73a50de1a16beb5e16a25f6c56e9d
greatscotts <javelonagian@live.com>
2013-10-03 00:04:27 +0800

    urltesting

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/facebook/';
+$config['base_url']	= 'http://campusorange.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
